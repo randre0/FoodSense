67948a2efb15dc661d9e26de6f575e9c4cf6d5af
randre0 <41939311+randre0@users.noreply.github.com>
2019-03-16 11:16:37 -0700

    Logging HR and Food Fixed

--- app/src/main/java/com/cs125/foodsense/LogFoodFragment.java
+++ app/src/main/java/com/cs125/foodsense/LogFoodFragment.java
@@ -1,5 +1,6 @@
 package com.cs125.foodsense;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,16 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cs125.foodsense.data.entity.FoodJournal;
+import com.cs125.foodsense.data.entity.HeartRate;
+import com.cs125.foodsense.data.util.Converters;
+import com.cs125.foodsense.data.util.Utility;
+import com.cs125.foodsense.data.view_model.FoodJournalViewModel;
+import com.cs125.foodsense.data.view_model.HeartRateViewModel;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static android.app.Activity.RESULT_OK;
 
 
@@ -25,16 +36,26 @@ public class LogFoodFragment extends Fragment {
     private int mAfterHeartRate;
     private String mFood;
     private String mCategory;
-    private View myView;
+    private View myView = null;
     private boolean heartRateClick;
     private boolean logfoodClick;
+    private HeartRateViewModel vm_heart_rate;
+    private FoodJournalViewModel vm_food_journal;
+    private LogFoodFragment myFragment;
+    private final String MY_FRAGMENT_TAG = "logfoodfragment";
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if(myView != null){
+            if((ViewGroup) myView.getParent() != null)
+                ((ViewGroup) myView.getParent()).removeView(myView);
+            return myView;
+        }
         myView = inflater.inflate(R.layout.fragment_log_food, container, false);
-
+        vm_heart_rate = ViewModelProviders.of(this).get(HeartRateViewModel.class);
+        vm_food_journal = ViewModelProviders.of(this).get(FoodJournalViewModel.class);
         mLogHRButton = (ImageButton) myView.findViewById(R.id.log_heart_rate);
         mLogFoodButton = (ImageButton) myView.findViewById(R.id.log_food);
         mBeforeHeartRate = Integer.MIN_VALUE;
@@ -71,7 +92,6 @@ public class LogFoodFragment extends Fragment {
                 logfoodClick = true;
             }
         });
-
         return myView;
     }
 
@@ -82,17 +102,34 @@ public class LogFoodFragment extends Fragment {
             if(heartRateClick) {
                 if (resultCode == RESULT_OK) {
                     int result = data.getIntExtra("heartrate", 0);
-                    Toast.makeText(getActivity(), "HeartRate Recieved:",
-                            Toast.LENGTH_LONG).show();
                     if(mBeforeHeartRate == Integer.MIN_VALUE){
                         mBeforeHeartRate = result;
                     }else if(mAfterHeartRate == Integer.MIN_VALUE &&
                             mFood != null & mCategory != null){
                             mAfterHeartRate = result;
                             //Send to database
+                            HeartRate beforeHR = new HeartRate("default@uci.edu",
+                                    mBeforeHeartRate, "BEFORE");
+                            HeartRate afterHR = new HeartRate("default@uci.edu",
+                                    mAfterHeartRate, "AFTER");
+                            vm_heart_rate.insert(beforeHR);
+                            vm_heart_rate.insert(afterHR);
+                            FoodJournal entry = new FoodJournal("default@uci.edu",
+                                    mFood, Converters.toDateString(Utility.getCurrentDateTime()));
+                            entry.setHrDiff((double)(mBeforeHeartRate - mAfterHeartRate));
+                            vm_food_journal.insert(entry);
+                            mFood = null;
+                            mCategory = null;
+                        Toast.makeText(getActivity(), "HR and Food logged",
+                                Toast.LENGTH_LONG).show();
                     } else if(mAfterHeartRate == Integer.MIN_VALUE){
                         //Send to database as result
+                        Toast.makeText(getActivity(), "Result Logged",
+                                Toast.LENGTH_LONG).show();
                         mBeforeHeartRate = result;
+                        HeartRate beforeHR = new HeartRate("default@uci.edu",
+                                mBeforeHeartRate, "RESULT");
+                        vm_heart_rate.insert(beforeHR);
                     }
                     heartRateClick = false;
                 }
@@ -102,8 +139,6 @@ public class LogFoodFragment extends Fragment {
                         String result2 = data.getStringExtra("food");
                         mFood = result2;
                         mCategory = result;
-                        Toast.makeText(getActivity(), "Food Recieved:",
-                                Toast.LENGTH_LONG).show();
                         logfoodClick = false;
                     }
             }
